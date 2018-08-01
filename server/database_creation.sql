ad6dcec6ce309085a1862776d2ce3aae1a3da389
agimpel <agimpel@student.ethz.ch>
2018-08-01 21:21:17 +0200

    remove stats table and uid column in database structure

--- server/database_creation.sql
+++ server/database_creation.sql
@@ -4,18 +4,8 @@ USE vcs_automat;
 DROP TABLE IF EXISTS archive;
 CREATE TABLE archive (
   id int unsigned NOT NULL AUTO_INCREMENT UNIQUE,
-  uid varchar(255) NOT NULL,
   unixtime int unsigned NOT NULL,
   time varchar(255) NOT NULL,
-  slot int NOT NULL,
-  PRIMARY KEY (id)
-);
-
-
-DROP TABLE IF EXISTS stats;
-CREATE TABLE stats (
-  id int unsigned NOT NULL AUTO_INCREMENT UNIQUE,
-  time int unsigned NOT NULL,
   slot int NOT NULL,
   PRIMARY KEY (id)
 );
@@ -25,13 +15,12 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id int unsigned NOT NULL AUTO_INCREMENT UNIQUE,
   uid varchar(255) NOT NULL UNIQUE,
-  credits int unsigned NOT NULL,
+  credits int NOT NULL,
   rfid varchar(255) NOT NULL UNIQUE,
-  tracking int NOT NULL DEFAULT 1,
   PRIMARY KEY (id)
 );
 LOCK TABLES users WRITE;
-INSERT INTO users VALUES (1,'default',100,'000000',1);
+INSERT INTO users VALUES (1,'default',100,'000000');
 UNLOCK TABLES;
 
 
@@ -50,6 +39,6 @@ CREATE TABLE settings (
     PRIMARY KEY (id)
 );
 LOCK TABLES settings WRITE;
-INSERT INTO settings VALUES (1,'frontend_active','0'),(2,'api_active','0'),(3,'standard_credits','2'),(4,'reset_interval','7'),(5,'last_reset','0'),(6,'next_reset','0');
+INSERT INTO settings VALUES (1,'frontend_active','0'),(2,'api_active','0'),(3,'standard_credits','1'),(4,'reset_interval','7'),(5,'last_reset','0'),(6,'next_reset','0');
 UNLOCK TABLES;
 
