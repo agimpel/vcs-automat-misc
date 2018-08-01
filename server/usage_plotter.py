f783c3601276c3ef54c2cecef26d23aa97db214c
agimpel <agimpel@student.ethz.ch>
2018-08-01 19:07:39 +0200

    fixed year plotting

--- server/usage_plotter.py
+++ server/usage_plotter.py
@@ -35,12 +35,7 @@ logger = logging.getLogger(logtitle)
 logger.setLevel(loglevel)
 
 
-
-def monday_of_week_one(yyyy):
-    REF_DAY = date(yyyy, 1, 4)
-    DOW = REF_DAY.weekday()
-    MONDAY = REF_DAY - timedelta(days = DOW)
-    return MONDAY
+STARTYEAR = 2017
 
 
 def set_title(plt, text):
@@ -50,80 +45,67 @@ def set_title(plt, text):
 
 
 
-months = mdates.MonthLocator()  # every month
+months = mdates.MonthLocator()
 monthsFmt = mdates.DateFormatter('1. %b')
 
-# Load a numpy record array from yahoo csv data with fields date, open, close,
-# volume, adj_close from the mpl-data/example directory. The record array
-# stores the date as an np.datetime64 with a day unit ('D') in the date column.
+raw_data = [random.randint(1483228800,1533112129) for i in range(900)]
 
-r = []
-for i in range(900):
-    r.append(random.randint(1483228800,1533112129))
+data = [{'hour': int(i.strftime('%H')), 'weekday': int(i.strftime('%w')), 'week': int(i.strftime('%W')), 'year': int(i.strftime('%Y'))} for i in map(datetime.fromtimestamp, raw_data)]
 
-data = []
-for i in r:
-    i_date = datetime.fromtimestamp(i)
-    data.append({'hour': i_date.strftime('%H'), 'weekday': i_date.strftime('%w'), 'week':i_date.strftime('%W'), 'day': i_date.strftime('%d'), 'month':i_date.strftime('%m'), 'year':i_date.strftime('%Y')})
+hours = np.array([sum(entry.get('hour') == i for entry in data) for i in range(0,23+1)])
+weekdays = np.array([sum(entry.get('weekday') == i for entry in data) for i in range(0,6+1)])
+weekyears = np.array([[sum(entry.get('year') == i and entry.get('week') == j for entry in data) for j in range(0,56+1)] for i in range(STARTYEAR, datetime.now().year+1)])
 
 
-hours = []
-for i in range(0,23+1):
-    hours.append(sum(int(entry.get('hour')) == i for entry in data))
 
-
-weekdays_title = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag']
-weekdays = []
-for i in range(0,6+1):
-    weekdays.append(sum(int(entry.get('weekday')) == i for entry in data))
-
-
-weekyears = np.zeros((datetime.now().year+1-2017, 54+1))
-for i in range(2017, datetime.now().year+1):
-    for j in range(0,54+1):
-        weekyears[i-2017, j] = sum((int(entry.get('year')) == i and int(entry.get('week')) == j) for entry in data)
-
-
+#
+# BASED ON WEEKDAYS
+#
 fig, ax = plt.subplots()
-ax.bar(weekdays_title, [x/sum(weekdays)*100 for x in weekdays])
+weekdays_title = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag']
+ax.bar(weekdays_title, weekdays/sum(weekdays)*100)
 fig.autofmt_xdate()
 plt.ylabel('relativer Konsum [%]')
 set_title(plt, 'Durchschnittlicher Konsum nach Wochentag')
-plt.show()
+plt.savefig('weekday.svg')
 
 
+#
+# BASED ON HOUR OF THE DAY
+#
 fig, ax = plt.subplots()
-ax.bar(range(0,23+1), [x/sum(hours)*100 for x in hours], align='edge')
+ax.bar(range(0,23+1), hours/sum(hours)*100, align='edge')
 ax.set_xlim(0, 24)
 ax.set_xticks([1,2,4,5,7,8,10,12,14,16,17,19,20,22,23], minor=True)
 plt.xticks((0,3,6,9,12,15,18,21,24))
 plt.ylabel('relativer Konsum [%]')
 set_title(plt, 'Durschnittlicher Konsum nach Uhrzeit')
-plt.show()
+plt.savefig('hour.svg')
 
-
-
-for i in range(2017, datetime.now().year+1):
+#
+# WEEKLY CONSUMPTION EACH YEAR
+#
+for i in range(STARTYEAR, datetime.now().year+1):
     fig, ax = plt.subplots()
-    weeknumber_start = int(datetime(i, 1, 1).strftime('%W'))
-    weeknumber_end = int(datetime(i, 12, 31).strftime('%W'))
 
-    if weeknumber_start is 0:
-        x = range(int(mdates.date2num(monday_of_week_one(i)))-7, int(mdates.date2num(datetime(i, 12, 31))), 7)
-    else:
-        x = range(int(mdates.date2num(monday_of_week_one(i))), int(mdates.date2num(datetime(i, 12, 31)))+7, 7)
-    ax.bar(x, weekyears[i-2017, weeknumber_start:weeknumber_end+1], width=6, align='edge')
+    first_day = date(i, 1, 1)
+    last_day = date(i, 12, 31)
+    monday_of_week_zero = first_day - timedelta(days = first_day.weekday())
+    weeknumber_start = 0 if first_day.weekday() > 0 else 1
+    weeknumber_end = int(last_day.strftime('%W'))
 
-    ax.xaxis.set_major_locator(months)
-    ax.xaxis.set_major_formatter(monthsFmt)
+    x = range(int(mdates.date2num(monday_of_week_zero)), int(mdates.date2num(last_day))+1, 7)
+    y = weekyears[i-STARTYEAR, weeknumber_start:weeknumber_end+1]
+    ax.bar(x, y, width=6, align='edge')
+
+    ax.xaxis.set_major_locator(mdates.MonthLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('1. %b'))
     fig.autofmt_xdate()
 
-    datemin = int(mdates.date2num(datetime(i, 1, 1)))
-    datemax = int(mdates.date2num(datetime(i, 12, 31)))
-    ax.set_xlim(datemin, datemax)
+    ax.set_xlim(int(mdates.date2num(datetime(i, 1, 1))), int(mdates.date2num(datetime(i, 12, 31))))
     plt.ylabel('Gesamtkonsum')
     set_title(plt, 'Wöchentlicher Konsum in '+str(i))
-    plt.show()
+    plt.savefig('year_'+str(i)+'.svg')
 
 
 
